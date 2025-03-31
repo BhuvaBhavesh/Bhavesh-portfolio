ea7a1625c5f031202b06b2e65ef6e28b0923a7bc
bhavesh bhuva <bbhuva294@gmail.com>
2025-03-31 17:41:00 +0530

    secoend-commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // 👈 Set this to your repository name!
+  base: "/Bhavesh-portfolio/",
+// 👈 Set this to your repository name!
 });
